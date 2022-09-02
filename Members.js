ebb743d3e4c8f52545584fc67ec01d495415bc0c
Reilly66 <reillyhowlett@icloud.com>
2022-09-02 11:04:01 +0100

    work for 1/9/22

---
+++ Members.js
@@ -0,0 +1,16 @@
+const members = [
+    {
+        id: 0,
+        name: "joe"
+    },
+    {
+        id: 1,
+        name: "david"
+    },
+    {
+        id: 2,
+        name: "bill"
+    }
+];
+
+module.exports = members;
